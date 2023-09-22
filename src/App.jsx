f8e6e562b996afe1d0b02d7706d80d9dd4e43be4
Antonio Servetti <antonio.servetti@polito.it>
2023-09-22 17:48:05 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const audioCtx = new AudioContext();
 
 function App() {
 
+  const [playing, setPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const audioEl = useRef(0);
+  // console.log(audioEl.current);
+
+  useEffect( () => {
+    audioEl.current.addEventListener('timeupdate', () => {
+      // console.log(audioEl.current.currentTime)
+      setCurrentTime(audioEl.current.currentTime);
+    })
+  }, [])
+
+
+  function handlePlayBtnClick() {
+    setPlaying(!playing);
+    if (playing) audioEl.current.pause();
+    else audioEl.current.play();
+  }
+
+  return (
+    <>
+      <div>
+        <p>
+        <audio id="audio" ref={audioEl} loop controls src="/singing.mp3" style={{ width: "50%" }}> </audio>
+        </p>
+      </div>
+      <div>
+        <p>
+        <button id="play" type="button" onClick={handlePlayBtnClick}>Play/Pause</button>
+        &nbsp;
+        <span>CurrentTime: {currentTime}</span>
+        </p>
+      </div>
+    </>
+  )
+
+  /*
   const defaultOsc1Frequency = 440;
 
   const [audioNodes, setAudioNodes] = useState({
@@ -22,8 +60,6 @@ function App() {
     if (ev.target.checked) audioCtx.resume();
     else audioCtx.suspend();
   }
-
-
   const [osc1Frequency, setOsc1Frequency] = useState(defaultOsc1Frequency);
 
   // executed every time a state changes
@@ -36,7 +72,6 @@ function App() {
         <label htmlFor="audioContextState">AudioContext state: {audioContextState}</label>
         <input type="checkbox" id="audioContextState" name="audioContextState"
           checked={audioContextState} onChange={(ev) => handleAudioContextStateChange(ev)} />
-        {/* audioCtx.state */ /* WARNING: state is not timely updated in the view */}
       </div>
       <div>
         <input type="range" id="osc1Frequency" name="osc1Frequency" min="220" max="1760" step="1" value={osc1Frequency}
@@ -45,6 +80,7 @@ function App() {
       </div>
     </>
   )
+  */
 }
 
 export default App
