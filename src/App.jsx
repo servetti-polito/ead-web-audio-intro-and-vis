307a46ea15c30217b641767ee223be93f7c521cc
Antonio Servetti <antonio.servetti@polito.it>
2023-09-22 10:27:00 +0200

    tentative implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+
+const audioCtx = new AudioContext();
 
 function App() {
 
+  const [audioContextState, setAudioContextState] = useState( audioCtx.state == "running"  )
+
+  function handleAudioContextStateChange(ev) {  
+    setAudioContextState(ev.target.checked);  
+    console.log(audioCtx);
+    if (ev.target.checked) {
+      console.log('resume');
+      audioCtx.resume() 
+     } else { 
+      audioCtx.suspend(); 
+      console.log('suspend');
+     }
+  }
+
   return (
     <>
-     <h1>EAD</h1>
+      <h1>EAD</h1>
+      <label htmlFor="audioContextState">AudioContext state: {audioContextState}</label>
+      <input type="checkbox" id="audioContextState" name="audioContextState"
+        checked={audioContextState.flag} onChange={ (ev) => handleAudioContextStateChange(ev) } />
+      {audioCtx.state}
     </>
   )
 }
