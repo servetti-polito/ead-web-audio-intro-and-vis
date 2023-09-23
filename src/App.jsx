fb5796f9e29ab8dac9803566d444e17b9e53e6ec
Antonio Servetti <antonio.servetti@polito.it>
2023-09-23 17:30:02 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from 'react';
 
+
+async function fetchAudioBuffer(url) {
+  let response = await fetch(url);
+  let arraybuffer = await response.arrayBuffer();
+  return await audioCtx.decodeAudioData(arraybuffer);
+}
+
 const audioCtx = new AudioContext();
 
 function App() {
@@ -7,57 +14,34 @@ function App() {
   const audioEl = useRef(0);
   // console.log(audioEl.current);
 
-  const [audioNodes, setAudioNodes] = useState({
-    audio1: null
-  })
+  const [audioNodes, setAudioNodes] = useState(null)
 
-  useEffect(() => { 
-    if(audioNodes.audio1 != null) {
-      console.log('set AudioNodes');
-      audioNodes.audio1.connect(audioCtx.destination);
-      // audioEl.current.play();
-    } else {
-      console.log('connect AudioNodes', audioNodes.audio1);
-      setAudioNodes( () => ({ audio1: new MediaElementAudioSourceNode(audioCtx, { mediaElement: audioEl.current }) }))
+  useEffect(() => {
+    const update = async () => {
+      if (audioNodes == null) {
+        console.log('set AudioNodes');
+        const impulseResponse = await fetchAudioBuffer('/1a_marble_hall.wav');
+        setAudioNodes(() => ({
+          audio1: new MediaElementAudioSourceNode(audioCtx, { mediaElement: audioEl.current }),
+          convolver1: new ConvolverNode(audioCtx, { buffer:  impulseResponse }),
+          gain1: new GainNode(audioCtx, { gain: 0.5 } )
+        }))
+      } else {
+        console.log('connect AudioNodes', audioNodes.audio1);
+        audioNodes.audio1.connect(audioNodes.convolver1).connect(audioNodes.gain1).connect(audioCtx.destination);
+        // audioEl.current.play();
+      }
     }
+    update();
   }, [audioNodes])
 
 
   const [audioContextState, setAudioContextState] = useState(audioCtx.state == "running")
-
   function handleAudioContextStateChange(ev) {
     setAudioContextState(ev.target.checked);
     if (ev.target.checked) audioCtx.resume();
     else audioCtx.suspend();
   }
-
-
-  const [playing, setPlaying] = useState(false);
-  function handlePlayBtnClick() {
-    setPlaying(!playing);
-    if (playing) audioEl.current.pause();
-    else audioEl.current.play();
-  }
-  
-  const [currentTime, setCurrentTime] = useState(0);
-  useEffect( () => {
-    audioEl.current.addEventListener('timeupdate', () => {
-      // console.log(audioEl.current.currentTime)
-      setCurrentTime(audioEl.current.currentTime);
-    })
-  }, [])
-
-  const audioTagWithButtonJSX = () => (
-    <>
-      <div> <p>
-        <audio id="audio" ref={audioEl} loop controls src="/singing.mp3" style={{ width: "50%" }}> </audio>
-      </p> </div>
-      <div> <p>
-        <button id="play" type="button" onClick={handlePlayBtnClick}>Play/Pause</button>
-      </p></div> 
-    </>
-  );
-  
   const audioContextCheckboxJSX = () => (
     <div>
       <label htmlFor="audioContextState">AudioContext state: {audioContextState}</label>
@@ -66,10 +50,56 @@ function App() {
     </div>
   );
 
+  const [playing, setPlaying] = useState(false);
+  function handlePlayBtnClick() {
+    setPlaying(!playing);
+    if (playing) audioEl.current.pause();
+    else audioEl.current.play();
+  }
+  const audioTagWithButtonJSX = () => (
+    <>
+      <div> <p>
+        <audio id="audio" ref={audioEl} loop controls src="/singing.mp3" style={{ width: "50%" }}> </audio>
+      </p> </div>
+      <div> <p>
+        <button id="play" type="button" onClick={handlePlayBtnClick}>Play/Pause</button>
+      </p></div>
+    </>
+  );
+
+
+
+  const [currentTime, setCurrentTime] = useState(0);
+  useEffect(() => {
+    audioEl.current.addEventListener('timeupdate', () => {
+      // console.log(audioEl.current.currentTime)
+      setCurrentTime(audioEl.current.currentTime);
+    })
+  }, [])
   const currentTimeJSX = () => (
     <div> <p>
-    <span>CurrentTime: {currentTime}</span>
+      <span>CurrentTime: {currentTime}</span>
     </p> </div>
+  );
+
+  const [convolverState, setConvolverState] = useState(true);
+  function handleConvolverStateChange(ev) {
+    setConvolverState(ev.target.checked);
+    if (ev.target.checked) {
+      audioNodes.audio1.disconnect();
+      audioNodes.audio1.connect(audioNodes.convolver1).connect(audioNodes.gain1).connect(audioCtx.destination);
+    } else {
+      // skip convolver
+      audioNodes.convolver1.disconnect(); audioNodes.audio1.disconnect();
+      audioNodes.audio1.connect(audioCtx.destination);
+    }
+  }
+  const convolverCheckboxJSX = () => (
+    <div>
+      <label htmlFor="convolverState">Convolver state: {audioContextState}</label>
+      <input type="checkbox" id="convolverState" name="convolverState"
+        checked={convolverState} onChange={handleConvolverStateChange} />
+    </div>
   );
 
 
@@ -78,50 +108,10 @@ function App() {
       {audioContextCheckboxJSX()}
       {audioTagWithButtonJSX()}
       {currentTimeJSX()}
+      {convolverCheckboxJSX()}
     </>
   )
 
-  /*
-  const defaultOsc1Frequency = 440;
-
-  const [audioNodes, setAudioNodes] = useState({
-    osc1: new OscillatorNode(audioCtx, { frequency: defaultOsc1Frequency })
-  })
-
-  useEffect(() => {
-    audioNodes.osc1.connect(audioCtx.destination);
-    audioNodes.osc1.start();
-  }, [])
-
-  const [audioContextState, setAudioContextState] = useState(audioCtx.state == "running")
-
-  function handleAudioContextStateChange(ev) {
-    setAudioContextState(ev.target.checked);
-    if (ev.target.checked) audioCtx.resume();
-    else audioCtx.suspend();
-  }
-
-  const [osc1Frequency, setOsc1Frequency] = useState(defaultOsc1Frequency);
-
-  // executed every time a state changes
-  audioNodes.osc1.frequency.value = osc1Frequency;
-
-  return (
-    <>
-      <h1>EAD</h1>
-      <div>
-        <label htmlFor="audioContextState">AudioContext state: {audioContextState}</label>
-        <input type="checkbox" id="audioContextState" name="audioContextState"
-          checked={audioContextState} onChange={(ev) => handleAudioContextStateChange(ev)} />
-      </div>
-      <div>
-        <input type="range" id="osc1Frequency" name="osc1Frequency" min="220" max="1760" step="1" value={osc1Frequency}
-          onChange={(ev) => setOsc1Frequency(ev.target.value)} />
-        <label htmlFor="osc1Frequency"> osc1Frequency: {osc1Frequency} </label>
-      </div>
-    </>
-  )
-  */
 }
 
 export default App
