6375dc41a376738419fc129915873d537d1c9375
Ale <notinmyhead@gmail.com>
2023-10-27 17:39:23 +0200

    Added Synthesis examples

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ Examples:
   - 000-audiocontext.jsx
   - 010-first_demo.jsx
   - 020-osc1_freq_slider.jsx
-  - 021-osc1_type_selection.jsx
-  - 022-osc1_with_custom_wave.jsx
+  - 021-osc_type_selection.jsx
+  - 022-osc_with_custom_wave.jsx
   - 030-audiotag.jsx
   - 040-audiotag_webaudio.jsx
   - 050-buffersource_synth.jsx
@@ -22,5 +22,7 @@ Examples:
   - 057-buffersource_pause.jsx
   - 110-audiotag_webaudio_analyser.jsx
   - 240-synthesis_additive_bell.jsx
+  - 241-synthesis_subtractive_square.jsx
+  - 242-synthesis_ring_modulator.jsx
   - 250-synthesis-fm-adv.jsx
 
---
+++ src/241-synthesis_subtractive_square.jsx
@@ -0,0 +1,81 @@
+/* eslint-disable react/prop-types */
+
+import {useState} from 'react';
+import './App.css';
+import {AudioContextComponent, Slider, ToggleTextButton} from './Tools.jsx'
+import {AudioAnalyser} from "./AudioAnalyser.jsx";
+
+const audioCtx = new AudioContext();
+
+function App() {
+
+    const defaultOscFrequency = 440;
+    const defaultFilterFrequency = 1000;
+    const defaultQValue = 25;
+    const [audioNodes, setAudioNodes] = useState({});
+    const [oscFrequency, setOscFrequency] = useState(defaultOscFrequency);
+    const [filterFrequency, setFilterFrequency] = useState(defaultFilterFrequency);
+
+    function start() {
+        const analyser = new AnalyserNode(audioCtx);
+        // envelope node to control the decreasing of sound
+
+        const osc = (new OscillatorNode(audioCtx, {frequency: oscFrequency }));
+        osc.type = "square";
+        osc.start();
+
+        const filter = audioCtx.createBiquadFilter();
+        filter.type = "lowpass";
+        filter.frequency.value = filterFrequency;
+        filter.Q.value = defaultQValue;
+
+        osc.connect(filter);
+        filter.connect(audioCtx.destination);
+        filter.connect(analyser);
+        setAudioNodes((audioNodes) => ({...audioNodes, osc, filter, analyser}))
+    }
+
+    function stop() {
+        audioNodes.osc.disconnect();
+
+        setAudioNodes((audioNodes) => ({...audioNodes, osc: null, filter: null}))
+    }
+
+
+    // executed every time a state changes
+    if (audioNodes?.osc) {
+        if (audioNodes.osc.frequency.value !== oscFrequency)
+            audioNodes.osc.frequency.value = oscFrequency;
+
+        if (audioNodes.filter.frequency.value !== filterFrequency)
+            audioNodes.filter.frequency.value = filterFrequency;
+
+    }
+
+    return (
+        <>
+            <h1>Oscillator with frequency slider and type selector</h1>
+            <p></p>
+            <AudioContextComponent audioCtx={audioCtx}/>
+            <p></p>
+            <ToggleTextButton handleClick={[start, stop]} text={["Start", "Stop"]}/>
+            <p></p>
+            <Slider label={"Osc Frequency"}
+                    name="oscFrequency" min={220} max={3520} step={1}
+                    value={oscFrequency}
+                    handleChange={(ev) => setOscFrequency(ev.target.value)}
+            />
+            <p></p>
+            <Slider label={"Filter Frequency"}
+            name="filterFrequency" min={0} max={10000} step={100}
+            value={filterFrequency}
+            handleChange={(ev) => setFilterFrequency(ev.target.value)}
+            />
+            <p></p>
+            <AudioAnalyser analyser={audioNodes?.analyser} sampleRate={audioCtx?.sampleRate}/>
+        </>
+    )
+}
+
+
+export default App
