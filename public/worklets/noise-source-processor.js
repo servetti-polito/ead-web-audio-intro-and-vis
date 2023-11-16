12319b7e60240f525d92bea0705d114c0158639a
Antonio Servetti <antonio.servetti@polito.it>
2023-11-16 17:46:49 +0100

    Add gain parameter

--- public/worklets/noise-source-processor.js
+++ public/worklets/noise-source-processor.js
@@ -1,14 +1,22 @@
 
 class NoiseSourceProcessor extends AudioWorkletProcessor {
-  constructor (options) { super(); }
+
+  static get parameterDescriptors() {
+    return [  {name: 'gain', defaultValue: 1 } ];
+  }
+
+  constructor (options) { super( options ); }
+
   process(inputs, outputs, parameters) {
+    console.log('processor gain:', parameters["gain"][0])
     for (let ch = 0; ch < outputs[0].length; ++ch) {
       for (let i = 0; i < outputs[0][ch].length; i++) {
-        outputs[0][ch][i] = Math.random() * 2 - 1;
+        outputs[0][ch][i] = (Math.random() * 2 - 1 ) * parameters["gain"][0];
       } 
     }
    return true;
   }
+
 }
 
 registerProcessor('NoiseSourceProcessor',NoiseSourceProcessor);
--- src/310-audioworklet-noise-source.jsx
+++ src/310-audioworklet-noise-source.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { AudioContextComponent, OneTimeButton } from './Tools.jsx'
+import { AudioContextComponent, OneTimeButton, Slider } from './Tools.jsx'
 import { AudioAnalyser }  from './AudioAnalyser.jsx'
 
 const audioCtx = new AudioContext();
@@ -8,12 +8,18 @@ await audioCtx.audioWorklet.addModule('/worklets/noise-source-processor.js')
 
 function App() {
 
-  const [audioNodes, setAudioNodes] = useState(null)
+  const defaultNoiseSourceGain = 0.5;
+  const [audioNodes, setAudioNodes] = useState(null);
+  const [noiseSourceGain, setNoiseSourceGain] = useState(defaultNoiseSourceGain);
+
+
 
   useEffect(() => {
     const update = async () => {
 
-      const customNode = new AudioWorkletNode(audioCtx, 'NoiseSourceProcessor', {} );
+      const customNode = new AudioWorkletNode(audioCtx, 'NoiseSourceProcessor', {
+        parameterData: { gain: 1 }
+      } );
       const analyser = new AnalyserNode(audioCtx);
 
       // connect
@@ -29,13 +35,21 @@ function App() {
   // we could use radio buttons to synth other frequencies in the buffer
   // new freq sill start when the buffer is re-read for the next loop
 
+  if(audioNodes?.customNode) audioNodes.customNode.parameters.get("gain").value = noiseSourceGain;
+
 
   return (
     <>
       <AudioContextComponent audioCtx={audioCtx} />
       <p></p>
       <OneTimeButton handleClick={() => audioCtx.resume()}>Start NoiseSourceProcessor</OneTimeButton>
-      <p></p>
+      <p></p>  
+      <Slider label="Noise Source Gain"
+        name="noiseSourceGain" min="0" max="2" step="any" 
+        value={noiseSourceGain}
+        handleChange={(ev) => setNoiseSourceGain(ev.target.value)}
+      />
+       <p></p>
       <AudioAnalyser analyser={audioNodes?.analyser} sampleRate={audioCtx?.sampleRate}/>
     </>
   )
